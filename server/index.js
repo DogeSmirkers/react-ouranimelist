6afe96ffd20fad7f12970ecb8d269dfe8b82bee1
LuongMonica <monica.luong.234@my.csun.edu>
2022-04-12 14:51:37 -0700

    updated readme, added docs, installed pkgs for backend

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
-var AWS = require("aws-sdk");
+// comment out aws stuff for now
+// var AWS = require("aws-sdk");
 
-AWS.config.update({
-  region: "us-east-1",
-  endpoint: "http://localhost:8000"
-});
+// AWS.config.update({
+//   region: "us-east-1",
+//   endpoint: "http://localhost:8000"
+// });
+
+const express = require('express');
+const path = require('path');
+const axios = require('axios');
+const {client_id} = require('./config');
+const app = express();
+const port = 4000;
+
+app.listen(port, () => {
+  console.log(`OurAnimeList is listening at http://localhost:${port}`);
+})
