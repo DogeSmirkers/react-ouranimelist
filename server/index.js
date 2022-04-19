9d27b03a32b421e83ec6e2d5aaf3ea63fc254b25
LuongMonica <monica.luong.234@my.csun.edu>
2022-04-19 16:25:28 -0700

    finally got res.send to send back API data

--- server/index.js
+++ server/index.js
@@ -11,7 +11,12 @@ const path = require('path');
 const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 4000;
-
+const {client_id} = require('./config');
+const API = require('./myanimelist-api-v2')
+const anime = new API.API_ANIME(client_id);
+const structures = require("./myanimelist-api-v2/src/Mal_Api_Authorized/Mal_Api_Anime/structures.json");
+// import conversion functions
+const tools = require('./Tools')
 //app.use(express.static('../client/public'))
 
 app.get('/', (req, res) => {
@@ -25,11 +30,30 @@ app.get('/search', (req, res) => {
     res.send(search.Search(query))
 })
 
-const getDetails = require('./GetDetails')
+// function GetAnimeDetails (id, fields=structures.animeFull) {
+//     //@params: id, fields
+//     const main = anime.anime(id, fields)
+//     .then((data) => {
+//         return data; 
+//     });
+
+//     // const printDetails = async () => {
+//     //     main.then((a) => {
+//     //         console.log(a);
+//     //     });
+//     // }
+//     // printDetails();
+// }
+
+//const getDetails = require('./GetDetails')
 // get anime details 
 app.get('/anime/:id', (req, res) => {
     const id = req.params.id
-    res.send(getDetails.GetDetails(id))
+    
+    const main = anime.anime(id, fields=structures.animeFull)
+    .then((data) => {
+        res.status(200).send(data)
+    });
 })
 
 const getSeasonal = require('./GetSeasonal')
@@ -38,7 +62,8 @@ app.get('/seasonal', (req, res) => {
     res.send(getSeasonal.GetSeasonal())
 })
 
-const getRanking = require('./GetRanking')
+const getRanking = require('./GetRanking');
+const GetDetails = require('./GetDetails');
 // get anime ranking
 app.get('/ranking', (req, res) => {
     let data = getRanking.GetRanking()
