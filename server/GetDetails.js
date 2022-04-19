031acb68badf8624c913070f13894eafd50bd819
LuongMonica <monica.luong.234@my.csun.edu>
2022-04-19 15:46:04 -0700

    able to print API returned data later; can only access return val of promise obj within a promise

--- server/GetDetails.js
+++ server/GetDetails.js
@@ -10,39 +10,49 @@ module.exports = {
     GetDetails: function (id, fields=structures.animeFull) {
         //@params: id, fields
         const main = anime.anime(id, fields)
-        .then(result => {
+        .then((data) => {
+            return data; 
+        });
 
-            // set each data field to var if it exists
-            title = tools.setData(result.title, result);
-            if (typeof result.main_picture !== "undefined" && typeof result.main_picture.medium !== "undefined") {let main_pic_med = result.main_picture.medium}
-            if (typeof result.main_picture !== "undefined" && typeof result.main_picture.large !== "undefined") {let main_pic_large = result.main_picture.large}
-            start_date = tools.setData(result.start_date);
-            end_date = tools.setData(result.end_date);
-            synopsis = tools.setData(result.synopsis);
-            currStatus = tools.setData(result.status);
-            num_ep = tools.setData(result.num_episodes);
-            genresObj = tools.setData(result.genres);
-            studiosObj = tools.setData(result.studios);
-            picObj = tools.setData(result.pictures);
-            mean = tools.setData(result.mean);
+        const printDetails = async () => {
+            main.then((a) => {
+                console.log(a);
+            });
+        }
+        printDetails()
+        // .then(result => {
 
-            // convert some data to more readable formats 
-            start_date = tools.convertIfExists(start_date);
-            end_date = tools.convertIfExists(end_date);
-            if (typeof currStatus !== "undefined") { currStatus = tools.convertStatus(currStatus)}
-            if (typeof synopsis !== "undefined") {synopsis = synopsis.split('[')[0]}
-            genres = tools.convertIfExists(genresObj);
-            studios = tools.convertIfExists(studiosObj);
+        //     // set each data field to var if it exists
+        //     title = tools.setData(result.title, result);
+        //     if (typeof result.main_picture !== "undefined" && typeof result.main_picture.medium !== "undefined") {let main_pic_med = result.main_picture.medium}
+        //     if (typeof result.main_picture !== "undefined" && typeof result.main_picture.large !== "undefined") {let main_pic_large = result.main_picture.large}
+        //     start_date = tools.setData(result.start_date);
+        //     end_date = tools.setData(result.end_date);
+        //     synopsis = tools.setData(result.synopsis);
+        //     currStatus = tools.setData(result.status);
+        //     num_ep = tools.setData(result.num_episodes);
+        //     genresObj = tools.setData(result.genres);
+        //     studiosObj = tools.setData(result.studios);
+        //     picObj = tools.setData(result.pictures);
+        //     mean = tools.setData(result.mean);
+
+        //     // convert some data to more readable formats 
+        //     start_date = tools.convertIfExists(start_date);
+        //     end_date = tools.convertIfExists(end_date);
+        //     if (typeof currStatus !== "undefined") { currStatus = tools.convertStatus(currStatus)}
+        //     if (typeof synopsis !== "undefined") {synopsis = synopsis.split('[')[0]}
+        //     genres = tools.convertIfExists(genresObj);
+        //     studios = tools.convertIfExists(studiosObj);
             
-            // loop through json response
-            let obj = result;
-            let res = [];
-            for(let i in obj) {
-                res.push([i, obj[i]]);
-            }
-            for(let i=0; i< res.length; i++) {
-                //console.log(res[i]);
-            }
-        }).catch(console.log)
+        //     // loop through json response
+        //     let obj = result;
+        //     let res = [];
+        //     for(let i in obj) {
+        //         res.push([i, obj[i]]);
+        //     }
+        //     for(let i=0; i< res.length; i++) {
+        //         console.log(res[i]);
+        //     }
+        // }).catch(console.log)
     }
 }
