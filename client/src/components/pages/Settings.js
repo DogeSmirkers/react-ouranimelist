80855fca7d7235f1cf8c638e7ce863e2b3eeef58
yelu2021 <ye.lu.273@my.csun.edu>
2022-04-21 20:54:31 -0700

    added Top anime page, Most Popular anime page and settings page

--- client/src/components/pages/Settings.js
+++ client/src/components/pages/Settings.js
@@ -1,12 +1,32 @@
 import React from 'react';
 // import '../../App.css';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export default function Settings() {
   return (
     <>
       <Container>
-          <h1>Settings</h1>
+          <h1 style={{ textAlign: 'center' }}>Settings </h1>
+          <div className='random-anime'>
+            <Row>
+              <Col lg={3}>
+                <Link to="">
+                  {/* <img src={require("../images/naruto.jpg")} alt="Card image" width={ '100%' }/> */}
+                  Profile Image
+                </Link>
+              </Col>
+              <Col lg={9}>
+                <h3> DogeSmirker_CoCo </h3>
+                <br/><br/>
+                <Button variant="outline-custom">Edit Username</Button>
+                <br/><br/>
+                <Button variant="outline-custom">Change Password</Button>
+                <br/><br/>
+                <Button variant="outline-danger">Delete Account</Button>
+              </Col>
+            </Row>
+          </div>
       </Container>
     </>
   );
--- client/src/components/pages/TopAnime.js
+++ client/src/components/pages/TopAnime.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export default function TopAnime() {
   return (
     <>
       <Container>
-      <h1>Top Anime of All Time</h1>
+          <h1 style={{ textAlign: 'center' }}>Top Anime</h1>
+          <div className='random-anime'>
+            <Row>
+              <Col lg={3}>
+                <Link to=""><img src={require("../images/naruto.jpg")} alt="Card image" width={ '100%' }/></Link>
+              </Col>
+              <Col lg={9}>
+                <h4> Naruto Shippuden </h4>
+                <p>Naruto Shippuden is the second series of Naruto anime that follows the titular hero on his quest to become Hokage. Unlike the first series, Naruto is now older and has new teachers to help him through his adventure.</p>
+                <p>Episode count - 500</p> 
+              </Col>
+            </Row>
+          </div>
       </Container>
     </>
   );
