1506a9bae90032118157e9b6d94c08163d6fbf27
LuongMonica <monica.luong.234@my.csun.edu>
2022-04-23 13:03:26 -0700

    testing folder to have front end call GOT API

--- client/src/Testing/App.js
+++ client/src/Testing/App.js
@@ -1,22 +1,9 @@
 import React, { useState } from 'react';
-import './App.css';
+import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import TopAnime from './components/pages/TopAnime';
-import RandomAnime from './components/pages/RandomAnime';
-import AboutUs from './components/pages/AboutUs';
-import ContactUs from './components/pages/ContactUs';
-import SignUp from './components/pages/SignUp';
-import Home from './components/pages/Home';
-import Profile from './components/pages/Profile';
-import Settings from './components/pages/Settings';
-import MostPopular from './components/pages/MostPopular';
-import Ranking from './components/pages/Ranking';
-import Search from './components/pages/Search';
 
-import useFetch from "./Testing/UseFetch";
-import House from "./Testing/House";
+import useFetch from "./UseFetch";
+import House from "./House";
 function App() {
 
   let audio = new Audio("/bark.mp3");
