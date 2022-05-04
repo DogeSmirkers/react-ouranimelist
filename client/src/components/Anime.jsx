a736f77937bd1db343ff3091161a2e838b0bb0c7
yelu2021 <ye.lu.273@my.csun.edu>
2022-05-03 23:12:01 -0700

    Validated studios

--- client/src/components/Anime.jsx
+++ client/src/components/Anime.jsx
@@ -19,7 +19,7 @@ export default function Anime({ anime }) {
               {typeof anime.end_date != "undefined" ? <p><b>Aired:</b> {Tools.convertDate(anime.start_date)} - {Tools.convertDate(anime.end_date)} </p> : <p><b>Aired:</b> {Tools.convertDate(anime.start_date)} - Still Airing </p>}
               {typeof anime.mean != "undefined" ? <p><b>MAL Score:</b> {anime.mean} </p> : <p><b>MAL Score:</b> N/A </p>}
               <p><b>Genre(s):</b> {Tools.convertObj(anime.genres)} </p>
-              <p><b>Studio(s):</b> {Tools.convertObj(anime.studios)} </p>
+              {Object.keys(anime.studios) > 0 ? <p><b>Studio(s):</b> {Tools.convertObj(anime.studios)} </p> : <p><b>Studio(s):</b> None Listed </p>}
               <img src={require("./images/mal-icon.png")} alt="myanimelist" width='30px' height='30px'/>&nbsp;<a class="text-dark" href={'https:/myanimelist.net/anime/'+anime.id} target="_blank" id="mal-pic">Click here for more info </a>
             </Col>
         </Row>
