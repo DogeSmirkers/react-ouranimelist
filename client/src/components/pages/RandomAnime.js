1dbbc8b3208f1546b1752cb5862460f4bab19e6d
LuongMonica <monica.luong.234@my.csun.edu>
2022-05-12 20:22:29 -0700

    removed refresh button on random anime ❌🔃

--- client/src/components/pages/RandomAnime.js
+++ client/src/components/pages/RandomAnime.js
@@ -3,21 +3,14 @@ import '../../App.css';
 import { Container, Button, Row } from 'react-bootstrap';
 import GetRandom from '../GetRandom';
 import RandoAnime from "../RandoAnime";
-import { IoRefreshOutline } from "react-icons/io5";
 
 export default function RandomAnime() {
   const { data, setData } = GetRandom();
-  function refreshPage() {
-    window.location.reload(false);
-  }
 
   return (
     <>
       <p onLoad={(e) => setData({ ...data})}> </p>
       <Container>
-        <Row>
-          <Button onClick={refreshPage}> <IoRefreshOutline></IoRefreshOutline> </Button>
-        </Row>
         {Object.keys(data.results).length > 0 ? <RandoAnime anime={data.results} /> : null}
       </Container>
     </>
